1e958703653c4ac4893a2e7ea1385773f1a30667
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-25 08:58:54 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,96 @@
+fun main(){
+    //Write and invoke one function that takes in a random string and returns the
+    //following values: first character, last character, length of the string and
+    //whether it begins with a vowel or not. (6pts)
+    var string =("My school is AkiraChix")
+   // println(last character)
+
+
+
+
+}
+
+
+    //Create a function that takes in a password as a parameter. For a password to
+    //be valid it must meet the following conditions:
+    //Must be at least 8 characters long
+    // Must be at most 16 characters long
+    // Must not be “password”
+    // Must contain a digit Your function should determine whether the password provided is valid or not.
+    //The function returns true/false
+
+var password=( "My password is 56789567")
+
+  //fun password(return:true)
+
+  //else{
+
+     // return false
+
+
+
+
+
+
+    //Create a function that prints out multiples of 6 and 8 between 1 and 1000. For
+    //multiples of both 6 and 8 it should print out “Bingo!” (5pts)
+    //for (num in 1..1000) {
+        //if (num % 48 === 0)
+
+   // }
+     //else if ( * 6 == 0) {
+      //  println("Bingo")
+   // }
+   // if ( *8== 0) {
+       // println("Bingo")
+    //}
+
+       // println("Multiples")
+
+
+
+
+    //Create a class CurrentAccount with the following attributes: account number,
+    //account name, balance. It has the following functions:
+    // deposit(amount: Double) - Increments the balance by the amount
+    //deposited
+    //withdraw(amount: Double) - Decrements the balance by the amount
+    //withdrawn
+    //details() - Prints out the account number and balance and name in
+   //this format: “Account number x with balance y is operated by z” (6pts)
+class CurrentAccount( var name:String, var accountnumber:Double, var balance:String )
+  fun name= ("Ann")
+fun account
+
+
+
+    //Create another class SavingsAccount. It has the same functions and
+    //        attributes as the current account except for one attribute, withdrawals: Int.
+   // Withdrawals is a counter variable that is used to keep track of how many
+    //withdrawals have been made from the account in a year. The only other
+           // difference is that the savings account withdraw() method first checks if the
+    //number of withdrawals is less than 4 for it to allow one to withdraw money
+    //from the account. It also increments the withdrawals attribute after a
+            //successful withdrawal (6pts)
+class Savingaccount(var name:String,var accountnumber:Int,var balance:String)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
